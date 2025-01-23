e9caea5f8474b63ecc922f1b481b3552b83010d2
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-01-23 15:09:31 -0300

    first commit

--- src/main/java/com/example/retornosAPI/repositories/ProductRepository.java
+++ src/main/java/com/example/retornosAPI/repositories/ProductRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    // Metodo para buscar produtos pelo nome (case insensitive)
+    // Metodo para buscar produtos pelo nome (case insensitive   )
     List<ProductEntity> findByNameContainingIgnoreCase(String name);
 }
